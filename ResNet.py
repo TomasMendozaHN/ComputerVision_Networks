66293bc12787c9b5c3d5372ea45134395d1e139e
tmendozarias <tmendozarias@gmail.com>
2020-03-21 14:38:54 +0800

    Added ResNet network

--- ResNet.py
+++ ResNet.py
@@ -1,1 +1,13 @@
 import torch
+
+class ResidualBlock(torch.nn.module):
+    def __init__(self):
+        super(ResidualBlock, self).__init__()
+
+        self.conv_1 = torch.nn.Conv2d()
+
+
+
+class ResNet(torch.nn.module):
+    def __init__(self, stride=1, n_classes=1000):
+        super(ResNet, self).__init__()
